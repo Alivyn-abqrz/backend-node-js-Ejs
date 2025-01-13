732cf98151c73dc18942abae12e0bbb303cb374d
ageesea <abarquezalvin10@gmail.com>
2025-01-14 00:13:21 +0800

    add body param and query

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/user", (req, res, next) => {
-  console.log("test");
+router.get("/user/:id/:name", (req, res, next) => {
+//  param
+//   const {id, name} = req.params;
+//  console.log(`id :${id}
+// name: ${name}`)
+
+// query
+// const {title, age} = req.query;
+// console.log(`title: ${title} 
+//   age: ${age}`)
+
+  // body
+  const {name, age, hobbies, language} = req.body;
+  console.log(`name:${name}
+    age:${age}
+    hobbies:${hobbies}
+    language${language}`)
 });
 router.get("/com", (req, res , next) => {
   console.log("com");
