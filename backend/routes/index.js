b62579dd31ea6b2aad0b1cbe4d5d36a2cf9dd7b2
ageesea <abarquezalvin10@gmail.com>
2025-01-15 00:02:45 +0800

    connect frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,25 +1,47 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.get("/user/:id/:name", (req, res, next) => {
-//  param
-//   const {id, name} = req.params;
-//  console.log(`id :${id}
-// name: ${name}`)
+// // using get localhost:3001/first/user/852456/alvin?title=Nodejspractice&age=23
+// router.get("/user/:id/:name", (req, res, next) => {
+// //  req param
+// //   const {id, name} = req.params;
+// //  console.log(`id :${id}
+// // name: ${name}`)
 
-// query
-// const {title, age} = req.query;
-// console.log(`title: ${title} 
-//   age: ${age}`)
+// // req query
+// // const {title, age} = req.query;
+// // console.log(`title: ${title} 
+// //   age: ${age}`)
 
-  // body
-  const {name, age, hobbies, language} = req.body;
-  console.log(`name:${name}
-    age:${age}
-    hobbies:${hobbies}
-    language${language}`)
-});
-router.get("/com", (req, res , next) => {
-  console.log("com");
-});
-module.exports = router;
+//   // req body
+//   const {name, age, hobbies, language} = req.body;
+//   console.log(`name:${name}
+//     age:${age}
+//     hobbies: ${hobbies[0]}
+//     language: ${language[2]}`)
+
+// //res status
+// // res.status(404) 
+
+// // res json to front-end
+//     res.status(404).json({
+//       Message:`your name is ${name} and your age is ${age}, hobbies is ${hobbies[0]} and my language is ${language[2]}`
+//     })
+// });
+
+// // using post
+// router.post("/job", (req, res , next) => {
+//   try{
+//     const {id} = req.query;
+//     // throw Error(); localhost:3001/first/job?id=2000
+//     if(!id){
+//       res.status(400).json({message:"message id is missing"})
+//     }
+//     res.status(200).json({message:'Your request success!.', data :id })
+//   }catch(error){
+//     res.status(500).json({message:'your request failed!', error: error})
+//   }
+  
+// });
+
+// module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path =   require('path');
 
-const app = express()
+const app = express();
+
+// front end
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', "html");
+app.set("views" , path.join(__dirname, "../frontend/views"));
+
 // Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
-const indexRoute = require('./routes/index')
+const appRoute = require('./routes/app')
+app.use('/', appRoute)
+ 
+// const indexRoute = require('./routes')
 // routes
-app.use('/first',indexRoute );
+// app.use('/first',indexRoute );
 const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
-}) 
+}) ;
